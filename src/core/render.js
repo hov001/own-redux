905ed4b655b5730e218ef315893e19c89ad709b9
Hovhannes Kocharyan <hovkocharyan81@gmail.com>
2023-07-30 12:23:36 +0400

    feat: add rendering of counter

---
+++ src/core/render.js
@@ -0,0 +1,3 @@
+export function render(element, state) {
+  element.innerText = state;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+import { render } from "./core/render";
 import "./styles.css";
+
+const counterSpan = document.getElementById("counter");
+const incrementBtn = document.getElementById("increment");
+const decrementBtn = document.getElementById("decrement");
+const timeoutBtn = document.getElementById("timeout");
+
+let counter = 0;
+
+render(counterSpan, counter);
+
+incrementBtn.addEventListener("click", () => {
+  counter += 1;
+  render(counterSpan, counter);
+});
+decrementBtn.addEventListener("click", () => {
+  counter -= 1;
+  render(counterSpan, counter);
+});
+timeoutBtn.addEventListener("click", () => {
+  setTimeout(() => {
+    counter += 1;
+    render(counterSpan, counter);
+  }, 2000);
+});
